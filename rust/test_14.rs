3c934b4df8506c7f9c4d6c89f9cd716a94f28c6e
Brent R. Brian <BrentRBrian@gmail.com>
2021-11-29 14:14:46 -0500

    assembly code example

--- rust/test_14.rs
+++ rust/test_14.rs
@@ -1,13 +1,19 @@
-fn main()
+#![feature(asm)]
+
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+
+fn main() 
 {
-    unsafe 
-    {
-        asm! 
-        {"
-            push %fs                
-            pip  %fs
-        "};
-    }
+  let x: u64;
+  
+  unsafe 
+  {
+    asm!("mov {}, 5", out(reg) x);
+  }
+
+  assert_eq!(x, 5);
+  
+  println!("value of x = {}",x);
 }
 
 
